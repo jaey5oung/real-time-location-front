bf1da1552430842380f78de67196d63ca09e0f06
jaey5oung <jaey5oung@naver.com>
2023-05-10 17:59:45 +0900

    feact leaflet socket connect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect } from "react"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import io from "socket.io-client"
-import { NaverMap, Marker, NavermapsProvider, MapDiv } from "react-naver-maps"
 
 const SOCKET_URL = "http://211.228.42.81:4000"
 
@@ -10,41 +10,31 @@ function App() {
   useEffect(() => {
     const socket = io(SOCKET_URL)
     socket.on("location", (location) => {
+      console.log("지역", location)
       setLocation(location)
     })
-    console.log(location)
 
     return () => {
       socket.disconnect()
     }
-  }, [])
+  }, [location])
+
+  const position = location
+    ? [location.latitude, location.longitude]
+    : [34.750231, 127.748656]
 
   return (
-    // <div style={{ height: "100vh" }}>
-    //<NavermapsProvider
-    //   ncpClientId="0enwpll4w2" // 자신의 네이버 계정에서 발급받은 Client ID
-    //   error={<p>Maps Load Error</p>}
-    //   loading={<p>Maps Loading...</p>}
-    // >
-    //   {" "}
-    //   <NaverMap
-    //     mapDivId={"react-naver-map"}
-    //     style={{
-    //       width: 800, // 네이버지도 가로 길이
-    //       height: 800, // 네이버지도 세로 길이
-    //     }}
-    //     defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
-    //   >
-    //     {location && (
-    //       <Marker
-    //         position={{ lat: location.latitude, lng: location.longitude }}
-    //       />
-    //     )}
-    //   </NaverMap>
-    // </NavermapsProvider>
-    <div>
-      <>d</>
-    </div>
+    <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+      />
+      {location && (
+        <Marker position={[location.latitude, location.longitude]}>
+          <Popup>Current Location</Popup>
+        </Marker>
+      )}
+    </MapContainer>
   )
 }
 
